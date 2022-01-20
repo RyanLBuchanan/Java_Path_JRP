bffb3de7cd425c5137b877ecf48cdf8b943365b6
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-01-20 09:47:47 -0700

    feat: Completed variables tutorial.

--- Getting_Started_with_Java/workspace_getting_started/java_basics/src/java_basics/Variables.java
+++ Getting_Started_with_Java/workspace_getting_started/java_basics/src/java_basics/Variables.java
@@ -3,8 +3,24 @@ package java_basics;
 public class Variables {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+//		Declaration
+		int x;
+//		Assignment
+		x = 42;
+		
+		System.out.println(x);
+		
+		int y = 69;
+		
+		System.out.println(x+y);
+		
+		String words;
+		
+		words = "Alita";
+		
+		System.out.println(words);
+		
 	}
 
 }
