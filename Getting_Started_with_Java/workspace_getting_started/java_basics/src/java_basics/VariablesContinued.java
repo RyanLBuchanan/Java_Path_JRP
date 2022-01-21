46641ce5d2edb8cadb8162777832f0151596a7e0
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-01-21 06:38:28 -0700

    feat: Added primitive datatypes.

--- Getting_Started_with_Java/workspace_getting_started/java_basics/src/java_basics/VariablesContinued.java
+++ Getting_Started_with_Java/workspace_getting_started/java_basics/src/java_basics/VariablesContinued.java
@@ -5,7 +5,12 @@ public class VariablesContinued {
 	public static void main(String[] args) {
 		int var = 2^31;
 		long bigNumber = 10000000000000000L;
+		short smallerBigNumber = -32767;
+		byte reallySmallNumber = 127;
 		
+		double decimalVar = 3.14159;
+		boolean decision = false; 
+		char letter = '9';
 	}
 
 }
