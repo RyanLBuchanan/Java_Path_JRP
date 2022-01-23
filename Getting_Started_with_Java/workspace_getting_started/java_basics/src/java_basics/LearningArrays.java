3d2298f190afaf21e766db0ea3a0228eb39e8a42
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-01-23 08:10:27 -0700

    feat: Completed array tutorial.

--- Getting_Started_with_Java/workspace_getting_started/java_basics/src/java_basics/LearningArrays.java
+++ Getting_Started_with_Java/workspace_getting_started/java_basics/src/java_basics/LearningArrays.java
@@ -3,8 +3,20 @@ package java_basics;
 public class LearningArrays {
 
 	public static void main(String[] args) {
-		int values;
-		values = 100;
+		
+		
+		// Initialize array -> a fixed size
+		double [] values = new double[100];
+		values[0] = 1000;
+		values[1] = 69;
+		values[99] = 314159;
+		
+		// Print array value
+		System.out.println(values[99]);
+		
+		String [] words = new String[] {"My", "Name", "is"};
+		
+		System.out.println(words[1]);
 	}
 
 }
