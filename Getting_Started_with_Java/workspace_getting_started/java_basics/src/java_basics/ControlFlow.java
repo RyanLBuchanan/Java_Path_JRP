45f2c753e426008b84412b55bbfa3fa4d347da8e
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-01-25 07:38:15 -0700

    feat: Saved control flow class.

--- Getting_Started_with_Java/workspace_getting_started/java_basics/src/java_basics/ControlFlow.java
+++ Getting_Started_with_Java/workspace_getting_started/java_basics/src/java_basics/ControlFlow.java
@@ -9,9 +9,32 @@ public class ControlFlow {
 		
 		if(hungry){
 			System.out.println("Must have sustenance!");
-		} else{
+		} else {
 			System.out.println("I'm good.");
 		}
+		
+		int hungerRating = 6;
+		if(!(hungerRating < 6)) {
+			System.out.println("Don't need fooder.");
+		} else {
+			System.out.println("Eat already?!");
+		}
+		
+		int favTemp = 75;
+		int curTemp = 32;
+		String opine;
+
+		if(curTemp < favTemp - 30) {
+			opine = "Damn, it cold!";
+		} else if (curTemp < favTemp - 20) {
+			opine = "No too cold, but must have layers.";
+		} else if (curTemp > favTemp + 10) {
+			opine = "Motherlover!  It's warm!";
+		} else {
+			opine = "Lovely day, Mate!";
+		}
+		
+		System.out.println(opine);
 		
 	}
 
