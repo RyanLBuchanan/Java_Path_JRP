ef43037aeab72ecabbcf6503d053a3b0d30328d9
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-01-28 11:28:21 -0700

    feat: Completed methods tutorial.

--- Getting_Started_with_Java/workspace_getting_started/java_basics/src/java_basics/LearningMethods.java
+++ Getting_Started_with_Java/workspace_getting_started/java_basics/src/java_basics/LearningMethods.java
@@ -4,12 +4,17 @@ public class LearningMethods {
 
 	public static void main(String[] args) {
 		
-		System.out.println("You must believe!");
+//		System.out.println("You must believe!");
 		
-		
-		MyUtils.printStuff("69");
-		MyUtils.printStuff(42);
+//		
+		System.out.println(MyUtils.printStuff("This is a cyberstring!"));
+//		
+//		MyUtils.printStuff(42);
+//		
+//		MyUtils.sum2Numbers(42, 69);
 
+//		int myVar = MyUtils.add10(42) + 1000 - 1000;
+//		System.out.println(myVar);
 	}
 	
 //	public static void printStuff(String argument, int argInt) {
--- Getting_Started_with_Java/workspace_getting_started/java_basics/src/java_basics/MyUtils.java
+++ Getting_Started_with_Java/workspace_getting_started/java_basics/src/java_basics/MyUtils.java
@@ -2,11 +2,22 @@ package java_basics;
 
 public class MyUtils {
 	
-	public static void printStuff(String argument) {
-		System.out.println("This is the string output: " + argument);
+	public static String printStuff(String argument) {
+//		System.out.println("This is the string output: " + argument);
+		return argument;
 	}
 	
-	public static void printStuff(int argument) {
+	public static int printStuff(int argument) {
 		System.out.println("This is the int output: " + argument);
+		return argument;
+	}
+	
+	public static void sum2Numbers(int firstArg, int secondArg) {
+		System.out.println(firstArg + secondArg);
+	}
+	
+	public static int add10(int someArg) {
+		int result = someArg + 10;
+		return result;
 	}
 }
