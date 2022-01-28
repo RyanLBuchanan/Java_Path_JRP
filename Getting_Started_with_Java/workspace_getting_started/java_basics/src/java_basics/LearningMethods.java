f28076ae3eca89bfd9ec5483a3189fc98ad9ee5c
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-01-28 11:04:07 -0700

    feat: Added MyUtils class.

--- Getting_Started_with_Java/workspace_getting_started/java_basics/src/java_basics/LearningMethods.java
+++ Getting_Started_with_Java/workspace_getting_started/java_basics/src/java_basics/LearningMethods.java
@@ -4,14 +4,17 @@ public class LearningMethods {
 
 	public static void main(String[] args) {
 		
-		System.out.println("");
+		System.out.println("You must believe!");
 		
 		
-		
-		
-		
-		
+		MyUtils.printStuff("69");
+		MyUtils.printStuff(42);
 
 	}
+	
+//	public static void printStuff(String argument, int argInt) {
+//		System.out.println("This is the output: " + argument + argInt);
+//	}
 
+	
 }
---
+++ Getting_Started_with_Java/workspace_getting_started/java_basics/src/java_basics/MyUtils.java
@@ -0,0 +1,12 @@
+package java_basics;
+
+public class MyUtils {
+	
+	public static void printStuff(String argument) {
+		System.out.println("This is the string output: " + argument);
+	}
+	
+	public static void printStuff(int argument) {
+		System.out.println("This is the int output: " + argument);
+	}
+}
