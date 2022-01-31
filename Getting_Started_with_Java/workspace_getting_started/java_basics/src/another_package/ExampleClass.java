0dd1682f3162bbc2c3b9211b93bb27019799c976
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-01-31 16:05:38 -0700

    feat: Refactored MyUtils.

---
+++ Getting_Started_with_Java/workspace_getting_started/java_basics/src/another_package/ExampleClass.java
@@ -0,0 +1,10 @@
+package another_package;
+
+import java_basics.MyUtils;
+
+public class ExampleClass {
+
+	public static void doSomething() {
+		MyUtils.printStuff(0);
+	}
+}
--- Getting_Started_with_Java/workspace_getting_started/java_basics/src/java_basics/LearningMethods.java
+++ Getting_Started_with_Java/workspace_getting_started/java_basics/src/java_basics/LearningMethods.java
@@ -10,17 +10,17 @@ public class LearningMethods {
 		System.out.println(MyUtils.printStuff("This is a cyberstring!"));
 //		
 		MyUtils.printStuff(42);
-//		MyUtils.printStuff(42);
-//		
-//		MyUtils.sum2Numbers(42, 69);
+		MyUtils.printStuff(42);
+		
+		MyUtils.sum2Numbers(42, 69);
 
-//		int myVar = MyUtils.add10(42) + 1000 - 1000;
-//		System.out.println(myVar);
+		int myVar = MyUtils.add10(42) + 1000 - 1000;
+		System.out.println(myVar);
 	}
 	
-//	public static void printStuff(String argument, int argInt) {
-//		System.out.println("This is the output: " + argument + argInt);
-//	}
+	public static void printStuff(String argument, int argInt) {
+		System.out.println("This is the output: " + argument + argInt);
+	}
 
 	
 }
