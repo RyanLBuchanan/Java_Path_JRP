808ff7a2df945c531f3cabb68aed8e839f76c57d
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-02-06 18:41:44 -0700

    feat: Commented out Ivan variables

--- Getting_Started_with_Java/workspace_getting_started/Prerequisite_OOP/src/Earth.java
+++ Getting_Started_with_Java/workspace_getting_started/Prerequisite_OOP/src/Earth.java
@@ -4,20 +4,20 @@ public class Earth {
 		
 		RussianHuman Иван;
 		
-		Иван = new RussianHuman();
-		// Ivan's age
-		Иван.возраст = 21;
-		// Ivan's eye color
-		Иван.цветглаз = "синий";
-		// Ivan's height
-		Иван.высота = 1.83;
-		// Ivan's name
-		Иван.название = "Иван Чернов"; 
-		
-		Иван.speak();
-		
-		
-		
+//		Иван = new RussianHuman();
+//		// Ivan's age
+//		Иван.возраст = 21;
+//		// Ivan's eye color
+//		Иван.цветглаз = "синий";
+//		// Ivan's height
+//		Иван.высота = 1.83;
+//		// Ivan's name
+//		Иван.название = "Иван Чернов"; 
+//		
+//		Иван.speak();
+//		
+//		
+//		
 		
 		
 		
