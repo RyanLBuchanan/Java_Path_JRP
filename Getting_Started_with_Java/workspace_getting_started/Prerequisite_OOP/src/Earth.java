a750a5a350e0629a717853b38518435a6b69265e
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-02-08 17:47:49 -0700

    feat: Added English speaking Human class

--- Getting_Started_with_Java/workspace_getting_started/Prerequisite_OOP/src/Earth.java
+++ Getting_Started_with_Java/workspace_getting_started/Prerequisite_OOP/src/Earth.java
@@ -4,17 +4,17 @@ public class Earth {
 		
 		RussianHuman Иван;
 		
-		Иван = new RussianHuman();
-		// Ivan's age
-		Иван.возраст = 21;
-		// Ivan's eye color
-		Иван.цветглаз = "синий";
-		// Ivan's height
-		Иван.высота = 1.83;
-		// Ivan's name
-		Иван.название = "Иван Чернов"; 
-		
-		Иван.russianSpeak();
+//		Иван = new RussianHuman();
+//		// Ivan's age
+//		Иван.возраст = 21;
+//		// Ivan's eye color
+//		Иван.цветглаз = "синий";
+//		// Ivan's height
+//		Иван.высота = 1.83;
+//		// Ivan's name
+//		Иван.название = "Иван Чернов"; 
+//		
+//		Иван.russianSpeak();
 		
 		Human Tpol = new Human();
 		Tpol.age = 69;
