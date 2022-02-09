0b5977a8a158e1029561a88dd6dacc619004982a
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-02-09 13:42:40 -0700

    feat: Added Vulcan class

--- Getting_Started_with_Java/workspace_getting_started/Prerequisite_OOP/src/Earth.java
+++ Getting_Started_with_Java/workspace_getting_started/Prerequisite_OOP/src/Earth.java
@@ -2,42 +2,14 @@
 public class Earth {
 	public static void main(String args[]) {
 		
-		RussianHuman Иван;
-		
-		Иван = new RussianHuman();
-		// Ivan's age
-		Иван.возраст = 21;
-		// Ivan's eye color
-		Иван.цветглаз = "синий";
-		// Ivan's height
-		Иван.высота = 1.83;
-		// Ivan's name
-		Иван.название = "Иван Чернов"; 
-		
-		Иван.russianSpeak();
-		
-		Vulcan Tpol = new Vulcan();
-		Tpol.age = 69;
-		Tpol.eyeColor = "Green";
-		Tpol.name = "Tpol VulcanSexy";
-		Tpol.height = 1.62;
-		
-		
-		Tpol.speak();
-		
-		
 		Human Archer = new Human();
-		Archer.age = 41;
-		Archer.eyeColor = "Blue";
-		Archer.name = "Captain Archer";
-		Archer.height = 1.72;
+		RussianHuman Ivan = new RussianHuman();
+		Vulcan TPol = new Vulcan();
 		
 		Archer.speak();
-		
-		
-	
-		
-		
-		
+		System.out.println("");
+		Ivan.russianSpeak();
+		System.out.println("");
+		TPol.speak();
 	}
 }
