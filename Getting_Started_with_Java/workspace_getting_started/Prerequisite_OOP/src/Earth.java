68806f19da531e381091c5dd77cd6b61462d3fbd
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-02-24 19:12:50 -0700

    feat: Added Vulcan class

--- Getting_Started_with_Java/workspace_getting_started/Prerequisite_OOP/src/Earth.java
+++ Getting_Started_with_Java/workspace_getting_started/Prerequisite_OOP/src/Earth.java
@@ -2,14 +2,14 @@
 public class Earth {
 	public static void main(String args[]) {
 		
-//		Human Archer = new Human();
-//		RussianHuman Ivan = new RussianHuman();
-//		Vulcan TPol = new Vulcan();
-//		
-//		Archer.speak();
-//		System.out.println("");
-//		Ivan.russianSpeak();
-//		System.out.println("");
-//		TPol.speak();
+		Human Archer = new Human();
+		RussianHuman Ivan = new RussianHuman();
+		Vulcan TPol = new Vulcan();
+		
+		Archer.speak();
+		System.out.println("");
+		Ivan.russianSpeak();
+		System.out.println("");
+		TPol.speak();
 	}
 }
