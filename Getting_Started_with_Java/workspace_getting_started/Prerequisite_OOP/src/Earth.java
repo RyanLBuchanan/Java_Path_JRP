8b508d1ae6a4a181f8630b41d0babc5fc42051b9
Ryan Buchanan <buchanan.ryan22@gmail.com>
2022-03-15 05:48:20 -0600

    feat: Removed RussianHuman class

--- Getting_Started_with_Java/workspace_getting_started/Prerequisite_OOP/src/Earth.java
+++ Getting_Started_with_Java/workspace_getting_started/Prerequisite_OOP/src/Earth.java
@@ -3,12 +3,12 @@ public class Earth {
 	public static void main(String args[]) {
 		
 		Human Archer = new Human("Archer", 42, 1.73, "blue");
-		RussianHuman Ivan = new RussianHuman("Ivan", 21, 1.83, "brown");
+//		RussianHuman Ivan = new RussianHuman("Ivan", 21, 1.83, "brown");
 		Vulcan TPol = new Vulcan("T'Pol", 69, 1.6, "green");
 		
 		Archer.speak();
 		System.out.println("");
-		Ivan.russianSpeak();
+//		Ivan.russianSpeak();
 		System.out.println("");
 		TPol.speak();
 	}
