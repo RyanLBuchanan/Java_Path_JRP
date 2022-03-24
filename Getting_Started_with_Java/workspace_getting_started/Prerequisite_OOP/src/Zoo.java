9e0f9e13769327569297e3359873910a395d1302
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2022-03-24 07:32:08 -0600

    feat: Added FishFace instantiation

--- Getting_Started_with_Java/workspace_getting_started/Prerequisite_OOP/src/Zoo.java
+++ Getting_Started_with_Java/workspace_getting_started/Prerequisite_OOP/src/Zoo.java
@@ -16,9 +16,9 @@ public class Zoo {
 		birdBrain.fly();
 		System.out.println("");
 		
-//		Fish fishFace = new Fish("FishFace", 1.1614, "Male", 42, "Rainbow");
-//		System.out.println("FishFace be...");
-//		fishFace.swim();
+		Fish fishFace = new Fish("FishFace", 1.1614, "Male", 42, "Rainbow");
+		System.out.println("FishFace be...");
+		fishFace.swim();
 	}
 
 }
